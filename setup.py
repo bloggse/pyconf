cc199f55afc383082858cc9c30a70d24377d2f93
Erik Jansson <erik@blogg.se>
2014-09-18 16:13:47 +0200

    make it work with python 3 as well, bump version to 0.4.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 d = {}
-execfile("pyconf.py", d, d)
+exec(compile(open("pyconf.py").read(), "pyconf.py", 'exec'), d, d)
 long_desc = d["__doc__"]
 
-setup(name="pyconf", version="0.4.1",
+setup(name="pyconf", version="0.4.2",
     url="http://blogg.se",
     description="Python-based configuration reader",
     long_description=long_desc,
     author="Blogg Esse AB",
-    author_email="opensource@blogg.se",
+    author_email="info@blogg.se",
     py_modules=["pyconf"])
